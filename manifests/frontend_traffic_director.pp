56e4a386da8a4ba6b2d78ad90d9c6ca7e0421af8
Noel Sharpe <noels@radnetwork.co.uk>
2013-12-20 16:38:39 +0000

    update beluga.

--- manifests/frontend_traffic_director.pp
+++ manifests/frontend_traffic_director.pp
@@ -3,5 +3,22 @@ class beluga::frontend_traffic_director(
   $lamp_servers,
   $lamp_admin_servers
 ) {
+  //create varnish backend for greylog, lamp and lam_admin
+
+  class { 'varnish::vcl':
+    probes => [
+      { name => 'health_check', url => "/health_check" },
+    ],
+    backends => [
+      { name => 'server1', host => '192.168.1.1', port => '80', probe => 'health_check' },
+      { name => 'server2', host => '192.168.1.2', port => '80', probe => 'health_check' },
+    ],
+    directors => [
+      { name => 'cluster', type => 'round-robin', backends => [ 'server1', 'server2' ] }
+    ],
+    selectors => [
+      { backend => 'cluster' },
+    ],
+  }
 
 }
