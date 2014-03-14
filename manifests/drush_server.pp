c867c68be84162445a20d07b819b1fec76cf6019
Noel Sharpe <noels@radnetwork.co.uk>
2014-03-14 16:19:13 +0000

    Add composer and drush to drush_server module.

--- manifests/drush_server.pp
+++ manifests/drush_server.pp
@@ -1,3 +1,22 @@
 class beluga::drush_server(){
 
+  class { 'composer': }
+
+  $drush_exec_dir = '/usr/local/bin'
+
+  exec { 'install-drush':
+    path      => ['/usr/bin', '/usr/sbin', '/bin', '/usr/local/bin'],
+    command   => "composer global require drush/drush:6.2",
+    onlyif    => "test ! -f ${drush_exec_dir}/drush",
+    require   => Class['composer'],
+  }
+
+  # Symlink drush executable
+  /*file { " ${drush_exec_dir}/drush":
+    ensure    => 'link',
+    target    => "${drush_exec_dir}/drush/drush",
+    owner     => $tomcat_user,
+    group     => $tomcat_group,
+  }*/
+
 }
