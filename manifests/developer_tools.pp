cb79682bd58da9147ae5d6b67c6af0fb7a1bd423
Noel Sharpe <noels@radnetwork.co.uk>
2014-03-17 17:10:31 +0000

    Add developer tools class to install grunt.

---
+++ manifests/developer_tools.pp
@@ -0,0 +1,13 @@
+class beluga::developer_tools(
+  $install_grunt = true,
+){
+
+  if ($install_grunt){
+    exec { 'install-grunt':
+      path      => ['/usr/bin', '/usr/sbin', '/bin', '/usr/local/bin'],
+      command   => "npm install -g grunt-cli",
+      require   => Class['nodejs'],
+    }
+  }
+
+}
