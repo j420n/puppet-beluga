b5f76b7d14f3e73efc04f3ce0c5e47e1c87c7417
Noel Sharpe <noels@radnetwork.co.uk>
2014-03-26 23:08:47 +0000

    Add postgresql.

--- manifests/postgresql_server.pp
+++ manifests/postgresql_server.pp
@@ -1,3 +1,14 @@
-class beluga::postgresql_server(){
+class beluga::postgresql_server (
+  $admin_password = "UNSET",
+){
+  if ("UNSET" == $admin_password){
+    $admin_pass = hiera("beluga::postgress_server::admin_password")
+  } else {
+    $admin_pass = $admin_password
+  }
+  notify {"Installing postgresql with a password of: $admin_pass": }
 
+  class { 'postgresql::server':
+    postgres_password          => $admin_pass,
+  }
 }
