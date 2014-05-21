e0ee1734c360068fb86dc933d48d6d4551be54a4
Noel Sharpe <noels@radnetwork.co.uk>
2014-05-21 17:16:24 +0100

    Add nodejs dependency to developer tools.

--- manifests/developer_tools.pp
+++ manifests/developer_tools.pp
@@ -3,6 +3,10 @@ class beluga::developer_tools(
 ){
 
   if ($install_grunt){
+    class { 'nodejs':
+      version => 'v0.10.26',
+    }
+
     exec { 'install-grunt':
       path      => ['/usr/bin', '/usr/sbin', '/bin', '/usr/local/bin'],
       command   => "/usr/local/node/node-default/bin/npm  install -g grunt-cli",
