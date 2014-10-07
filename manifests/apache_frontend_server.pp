b501c05ae24ed901eaeaa8851eb95eb7d0263f3b
Noel Sharpe <noels@radnetwork.co.uk>
2014-10-07 16:00:02 +0100

    Remove apche vhost config as it's wrong.

--- manifests/apache_frontend_server.pp
+++ manifests/apache_frontend_server.pp
@@ -1,8 +1,4 @@
 class beluga::apache_frontend_server(
-  $apache_port = 8000,
-  $domain_name = 'frontend',
-  $owner = 'beluga',
-  $group = 'beluga'
 ){
 
   #install and configure apache
@@ -16,15 +12,6 @@ class beluga::apache_frontend_server(
   include apache::mod::rewrite
 
   include beluga::php
-
-  #create a vhost for the frontend lamp server
-  apache::vhost { $domain_name:
-    override      => 'All',
-    port          => $apache_port,
-    docroot       => "/var/www/${domain_name}",
-    docroot_owner => $owner,
-    docroot_group => $group,
-  }
 
 }
 
