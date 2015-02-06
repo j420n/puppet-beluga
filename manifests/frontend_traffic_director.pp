7b537f5874d9ba242d3a51772e4e3af9e676e887
Noel Sharpe <noels@radnetwork.co.uk>
2015-02-06 17:05:17 +0000

    Fetd is so broken I will start again.

--- manifests/frontend_traffic_director.pp
+++ manifests/frontend_traffic_director.pp
@@ -14,40 +14,4 @@ class beluga::frontend_traffic_director(
 
   include 'nginx'
 
-  $ret = inline_template("<%= lamp_servers.each do |lamp_server| ${lamp_server['host']}:${lamp_server['upstream_port']},")
-  notify{$ret:}
-
-  nginx::resource::upstream {$lamp_servers:
-    ensure => present,
-    #members => inline_template("<%= lamp_servers.each do |lamp_server| \"${lamp_server['host']}:${lamp_server['upstream_port']},\"%>").split
-  }
-
-  nginx::resource::vhost {$frontend_domain:
-    ensure   => present,
-    listen_port => $nginx_port,
-    pproxy => "http://${$frontend_domain}",
-  }
-
-  #install and configure varnish for graylog, lamp and lamp_admin
-  class { 'varnish':
-    varnish_listen_port => $varnish_port
-  }
-
-  $all_backends = push([$lamp_servers, $lamp_admin_servers, $solr_servers], $extra_backends)
-  $selectors1 = [
-    { backend => 'solr', condition => 'req.http.host ~ "^solr."'},
-    { backend => 'lamp_admin_servers', condition => 'req.http.host ~ "^admin."'}]
-  $selectors2 = push($selectors1, $extra_selectors)
-  $all_selectors = push($selectors2, [ { backend => 'frontend' }])
-  class { 'varnish::vcl':
-    probes => [
-      #{ name => 'health_check', url => "/health_check" }
-    ],
-    backends => $all_backends,
-    directors => [
-      { name => 'frontend', type => 'round-robin', backends => $lamp_servers['name'] }
-    ],
-    selectors => $all_selectors,
-  }
-
 }
