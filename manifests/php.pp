b38102bbde733a8ff1607c20510e10cecde01b5f
Noel Sharpe <noels@radnetwork.co.uk>
2015-02-24 16:23:15 +0000

    Require apache f/e for installing apache php.ini.

--- manifests/php.pp
+++ manifests/php.pp
@@ -14,6 +14,7 @@ class beluga::php(
     ensure  => 'installed',
   }
   file {$beluga::params::php_ini:
-    content => template('beluga/php.ini.erb')
+    content => template('beluga/php.ini.erb'),
+    require => Class['apache_frontend_server'],
   }
 }
