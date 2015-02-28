29025471d2ea71102885a73826c505d04360ee01
Jason Brown <jason@silex-consulting.com>
2015-02-28 23:23:21 +0000

    Use a global install package parameter.

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,18 +1,32 @@
 class beluga::php(
   $memory_limit = $beluga::params::php_memory_limit,
 ) inherits beluga::params {
-  package { $beluga::params::php_package:
-    ensure => 'installed'
+
+  #you can use a global package parameter
+  Package { ensure => "installed" }
+  
+  case $::osfamily {
+    'RedHat': {
+
+    }
+    'Debian': {
+      package { $beluga::params::php_common_package: } ->
+      package { $beluga::params::php_gd_package: } ->
+      package { $beluga::params::php_cli_package: } ->
+      package { $beluga::params::php_cgi_package: } ->
+      package { $beluga::params::php_curl_package: } ->
+      package { $beluga::params::php_mcrypt_package: } ->
+      package { $beluga::params::php_postgres_package: }
+    }
+    default: {
+      fail("${::operatingsystem} not supported")
+    }
   }
+
   class {'mysql::bindings':
     php_enable         => true,
   }
-  package { $beluga::params::php_gd_package:
-    ensure   => 'installed',
-  }
-  package { $beluga::params::php_postrges_package:
-    ensure  => 'installed',
-  }
+  
   file {$beluga::params::php_ini:
     content => template('beluga/php.ini.erb'),
     require => Class['apache_frontend_server'],
