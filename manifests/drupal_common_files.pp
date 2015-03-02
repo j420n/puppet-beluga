2c7071f46de83a3b0a5f1f7138571c7151473011
Jason Brown <jason@silex-consulting.com>
2015-03-02 22:18:46 +0000

    Update drupal_common_files

--- manifests/drupal_common_files.pp
+++ manifests/drupal_common_files.pp
@@ -1,19 +1,19 @@
 class beluga::drupal_common_files() {
   file { "${private_file_dir}/":
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::private_file_dir_owner",'beluga'),
+    owner  => hiera("beluga::drupal_site::private_file_dir_owner", "${site_owner}"),
     group  => $web_group,
     mode   => 775,
   }
   file { "${drupal_file_dir}/":
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::drupal_file_dir_owner",'beluga'),
+    owner  => hiera("beluga::drupal_site::drupal_file_dir_owner", "${site_owner}"),
     group  => $web_group,
     mode   => 775,
   }
   file { "${drupal_site_dir}/":
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::drupal_site_dir_owner",'beluga'),
+    owner  => hiera("beluga::drupal_site::drupal_site_dir_owner", "${site_owner}"),
     group  => $web_group,
     mode   => 775,
   }
