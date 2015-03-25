de4b1aaa2c421e4f4571e693414afd2cbfb2d592
David Otton <david@otton.org>
2015-03-25 20:30:44 +0000

    Fix for variable reassignment in mysql_server

--- manifests/mysql_server.pp
+++ manifests/mysql_server.pp
@@ -2,11 +2,13 @@ class beluga::mysql_server (
   $admin_password = "UNSET",
 ){
   if ("UNSET" == $admin_password){
-    $admin_password = hiera("beluga::mysql_server::admin_password")
+    $tmp_admin_password = hiera("beluga::mysql_server::admin_password")
+  } else {
+    $tmp_admin_password = $admin_password
   }
 
   class { '::mysql::server':
-    root_password    => $admin_password,
+    root_password    => $tmp_admin_password,
     override_options => { 'mysqld' => { 'max_connections' => '1024' } }
   }
 }
