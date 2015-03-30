d0ba6a29ffa06e243e59d7740aa45d038b83e7aa
Jason Brown <jason@silex-consulting.com>
2015-03-30 16:58:08 +0100

    Set the default private file dir owner to beluga if it is not set in hiera.

--- manifests/drupal_common_files.pp
+++ manifests/drupal_common_files.pp
@@ -1,21 +1,24 @@
-class beluga::drupal_common_files() {
-  file { "${private_file_dir}/":
+class beluga::drupal_common_files(
+$pfd = hiera("beluga::drupal_site::private_file_dir",'/var/www/private'),
+$dfd = hiera("beluga::drupal_site::drupal_file_dir",'/var/www/files'),
+$dsd = hiera("beluga::drupal_site::drupal_file_dir",'/var/www/drupal'),
+) {
+  file { $pfd:
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::private_file_dir_owner", "${site_owner}"),
+    owner  => hiera("beluga::drupal_site::private_file_dir_owner",'beluga'),
     group  => $web_group,
     mode   => 775,
   }
-  file { "${drupal_file_dir}/":
+  file { $dfd :
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::drupal_file_dir_owner", "${site_owner}"),
+    owner  => hiera("beluga::drupal_site::drupal_file_dir_owner",'beluga'),
     group  => $web_group,
     mode   => 775,
   }
-  file { "${drupal_site_dir}/":
+  file { $dsd :
     ensure => "directory",
-    owner  => hiera("beluga::drupal_site::drupal_site_dir_owner", "${site_owner}"),
+    owner  => hiera("beluga::drupal_site::drupal_site_dir_owner",'beluga'),
     group  => $web_group,
     mode   => 775,
   }
-
 }
