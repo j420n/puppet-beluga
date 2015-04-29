461feedccdd85ce39a45fe93b045e8e22cb5a665
Jason Brown <jason@silex-consulting.com>
2015-04-29 18:39:22 +0100

    Change drupal file directory for hiera.

--- manifests/drupal_common_files.pp
+++ manifests/drupal_common_files.pp
@@ -1,7 +1,7 @@
 class beluga::drupal_common_files(
 $pfd = hiera("beluga::drupal_site::private_file_dir",'/var/www/private'),
 $dfd = hiera("beluga::drupal_site::drupal_file_dir",'/var/www/files'),
-$dsd = hiera("beluga::drupal_site::drupal_file_dir",'/var/www/drupal'),
+$dsd = hiera("beluga::drupal_site::drupal_site_dir",'/var/www/drupal'),
 ) {
   file { $pfd:
     ensure => "directory",
