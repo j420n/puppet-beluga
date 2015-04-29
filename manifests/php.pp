10c0e254a6bfe4b1bf26ee825a46e8155df34ff4
Jason Brown <jason@silex-consulting.com>
2015-04-29 17:11:52 +0100

    Use global package parameter to ensure php is installed.

--- manifests/php.pp
+++ manifests/php.pp
@@ -1,18 +1,18 @@
 class beluga::php(
   $memory_limit = $beluga::params::php_memory_limit,
 ) inherits beluga::params {
-  package { $beluga::params::php_package:
-    ensure => 'installed'
-  }
+
+  #you can use a global package parameter
+  Package { ensure => "installed" }
+
+  $packages = ["$beluga::params::php_package","$beluga::params::php_gd_package","$beluga::params::php_postgres_package"]
+
+  package{$packages:}
+
   class {'mysql::bindings':
     php_enable         => true,
   }
-  package { $beluga::params::php_gd_package:
-    ensure   => 'installed',
-  }
-  package { $beluga::params::php_postgres_package:
-    ensure  => 'installed',
-  }
+
   file {$beluga::params::php_ini:
     content => template('beluga/php.ini.erb'),
     require => Class['apache_frontend_server'],
