d06e82ca5aed0f49297dd39563aa12e52e504c5d
Jason Brown <jason@silex-consulting.com>
2015-04-29 17:47:26 +0100

    Add yum update to osfamily case for Redhat.

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,13 +14,32 @@ class beluga::install(){
     ensure   => 'installed',
   }
 
-  package { 'lsb-release':
-    ensure => installed,
+  case $::osfamily {
+    'RedHat': {
+
+      exec { "yum-update":
+        command => "/usr/bin/yum update"
+      }
+
+      Exec["yum-update"] -> Package <| |>
+
+    }
+    'Debian': {
+
+       exec { "apt-update":
+         command => "/usr/bin/apt-get update"
+       }
+
+       Exec["apt-update"] -> Package <| |>
+
+       package { 'lsb-release':
+         ensure => installed,
+       }
+
+    }
+    default: {
+       fail("${::operatingsystem} not supported")
+    }
   }
 
-  exec { "apt-update":
-    command => "/usr/bin/apt-get update"
-  }
-
-  Exec["apt-update"] -> Package <| |>
 }
