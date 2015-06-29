cb7f358234c4698a929937ced3104603569ff845
Jason Brown <jason@silex-consulting.com>
2015-06-29 12:46:41 +0100

    Remove setting admin password with hiera directly in beluga::mysql_server class, It only works if the password is in common.yaml.

--- manifests/mysql_server.pp
+++ manifests/mysql_server.pp
@@ -1,12 +1,6 @@
 class beluga::mysql_server (
-  $admin_password = "UNSET",
+  $admin_password = "default",
 ){
-  if ("UNSET" == $admin_password){
-    $tmp_admin_password = hiera("beluga::mysql_server::admin_password")
-  } else {
-    $tmp_admin_password = $admin_password
-  }
-
   class { '::mysql::server':
     root_password    => $admin_password,
     #If mysql override options are set here then we cant set them with hiera.
